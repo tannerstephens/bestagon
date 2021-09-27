9f06038a4386f24c7f4c149289e5c871f8febcca
Tanner Stephens <tannerrstephens@gmail.com>
2021-09-26 20:35:33 -0500

    Ripple test

---
+++ bestagon-worker/effects/ripple.py
@@ -0,0 +1,71 @@
+import math
+import time
+import random
+
+class Rip:
+  def __init__(self, led_map, start):
+    self.led_map = led_map
+    self.active = set(start)
+    self.seen = set()
+
+  def step(self):
+    self.seen.update(self.active)
+
+    new_active = set()
+
+    for p in self.active:
+      for q in [-1,0,1]:
+        for r in [-1,0,1]:
+          check = (p[0] + q, p[1] + r)
+          if check not in self.seen and check in self.led_map:
+            new_active.add(check)
+
+
+    self.active = new_active
+
+    for p in self.active:
+      yield self.led_map[p]
+
+  def is_empty(self):
+    return len(self.active) == 0
+
+
+
+class Ripple:
+  def __init__(self, pixels, led_map):
+    self.pixels = pixels
+    self.led_map = led_map
+    self.color = (255,255,255)
+
+    self.chance = 1
+    self.out_of = 100
+
+    self.ripples = []
+
+    self.decay_steps = 20
+
+    self.dr = math.ceil(self.color[0] // self.decay_steps + 0.5)
+    self.dg = math.ceil(self.color[1] // self.decay_steps + 0.5)
+    self.db = math.ceil(self.color[2] // self.decay_steps + 0.5)
+
+  def run(self):
+    for i in range(len(self.pixels)):
+      if self.pixels[i] != [0,0,0]:
+        p = self.pixels[i]
+        self.pixels[i] = (max(p[0] - self.dr,0),max(p[1] - self.dg,0),max(p[2] - self.db,0))
+
+    for r in self.ripples:
+      for p in r.step():
+        self.pixels[p] = self.color
+
+    for point in self.led_map:
+      if random.randint(0,self.out_of) < self.chance:
+        self.ripples.append(Rip(self.led_map, point))
+        self.pixels[self.led_map[point]] = self.color
+
+def register():
+  return {
+    'class': Ripple,
+    'name': 'Ripple',
+    'configs': {}
+  }
