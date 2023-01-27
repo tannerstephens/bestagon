55bca79632c4e408529b5527a28ec9e8b9d12456
Tanner Stephens <tannerrstephens@gmail.com>
2023-01-26 19:01:46 -0600

    Better image handling

--- bestagon-worker/effects/photo.py
+++ bestagon-worker/effects/photo.py
@@ -11,13 +11,24 @@ from math import sqrt
 class Photo(Effect):
   NAME = 'Photo'
 
+  def __init__(self, pixels, redis_conn):
+    super().__init__(pixels, redis_conn)
+
+    self.data_url = None
+
   def setup_config(self):
     self.config.add('Photo', 'image', str, '')
     self.config.add('Brightness', 'number', float, 0.5)
 
   def refresh_config(self):
     super().refresh_config()
-    self.data_url = self.config.get('Photo').value
+    new_data_url = self.config.get('Photo').value
+
+    if new_data_url != self.data_url:
+      self.data_url = new_data_url
+
+    self.image = self.get_image(self.data_url)
+
     self.brightness = self.config.get('Brightness').value
 
   def get_image(self):
@@ -26,13 +37,11 @@ class Photo(Effect):
     return Image.open(BytesIO(b64decode(base64)))
 
   def run(self):
-    image = self.get_image().resize((42,42))
-
     for (q,r) in led_map:
       x = (2 * (sqrt(3) * q + sqrt(3) / 2 * r))+21
       y = (2 * 1.5 * r)+18
 
-      (red, green, blue) = image.getpixel((x,y))[:3]
+      (red, green, blue) = self.image.getpixel((x,y))[:3]
       red = int(red * self.brightness)
       green = int(green * self.brightness)
       blue = int(blue * self.brightness)
@@ -40,4 +49,4 @@ class Photo(Effect):
       self.pixels[led_map[(q,r)]] = [red, green, blue]
 
     self.pixels.show()
-    time.sleep(1)
+    time.sleep(5)
